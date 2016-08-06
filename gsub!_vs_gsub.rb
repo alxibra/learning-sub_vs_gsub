22bc537b86f7e32c9f66ce1c6ab391b3f48917e8
alexander ibrahim <alxibra@gmail.com>
2016-08-06 08:58:20 +0700

    add comparation for gsub and gsub!

--- gsub!_vs_gsub.rb
+++ gsub!_vs_gsub.rb
@@ -14,3 +14,15 @@ Benchmark.ips do |x|
   end
   x.compare!
 end
+
+# warming up --------------------------------------
+#                 gsub     1.000  i/100ms
+#                gsub!     1.000  i/100ms
+# Calculating -------------------------------------
+#                 gsub      6.307  (± 0.0%) i/s -     32.000  in   5.074123s
+#                gsub!      6.202  (± 0.0%) i/s -     32.000  in   5.159736s
+
+# Comparison:
+#                 gsub:        6.3 i/s
+#                gsub!:        6.2 i/s - 1.02x slower
+
